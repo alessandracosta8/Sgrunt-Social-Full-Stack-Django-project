e35c9c08e9fca8e44a6d48266f2ac19b33774159
Alessandra Costa <alessandracosta8@gmail.com>
2022-08-23 10:19:12 +0000

    Test urls

---
+++ core/tests/test_urls.py
@@ -0,0 +1,51 @@
+""" Imports: """
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from core.views import (
+    PostListView,
+    PostDetailView,
+    PostEditView,
+    PostDeleteView,
+    CommentDeleteView,
+    ProfileView,
+    ProfileEditView
+)
+
+
+class TestUrls(SimpleTestCase):
+    """ core app urls testing """
+
+    def test_post_list_url_is_resolved(self):
+        """ check if url calls the right view """
+        url = reverse('post-list')
+        self.assertEqual(resolve(url).func.view_class, PostListView)
+
+    def test_post_detail_url_is_resolved(self):
+        """ check if url calls the right view """
+        url = reverse('post-detail', args=['1'])
+        self.assertEqual(resolve(url).func.view_class, PostDetailView)
+
+    def test_post_edit_url_is_resolved(self):
+        """ check if url calls the right view """
+        url = reverse('post-edit', args=['1'])
+        self.assertEqual(resolve(url).func.view_class, PostEditView)
+
+    def test_post_delete_url_is_resolved(self):
+        """ check if url calls the right view """
+        url = reverse('post-delete', args=['1'])
+        self.assertEqual(resolve(url).func.view_class, PostDeleteView)
+
+    def test_comment_delete_url_is_resolved(self):
+        """ check if url calls the right view """
+        url = reverse('comment-delete', args=['1', '1'])
+        self.assertEqual(resolve(url).func.view_class, CommentDeleteView)
+
+    def test_profile_url_is_resolved(self):
+        """ check if url calls the right view """
+        url = reverse('profile', args=['1'])
+        self.assertEqual(resolve(url).func.view_class, ProfileView)
+
+    def test_profile_edit_url_is_resolved(self):
+        """ check if url calls the right view """
+        url = reverse('profile-edit', args=['1'])
+        self.assertEqual(resolve(url).func.view_class, ProfileEditView)
--- core/urls.py
+++ core/urls.py
@@ -2,14 +2,25 @@
 Imports:
 """
 from django.urls import path
-from .views import PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView, ProfileView, ProfileEditView
-
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostEditView,
+    PostDeleteView,
+    CommentDeleteView,
+    ProfileView,
+    ProfileEditView
+)
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/edit/<int:pk>/', PostEditView.as_view(), name='post-edit'),
-    path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='post-delete'),
+    path(
+        'post/delete/<int:pk>/',
+        PostDeleteView.as_view(),
+        name='post-delete'
+        ),
     path(
         'post/<int:post_pk>/comment/delete/<int:pk>/',
         CommentDeleteView.as_view(),

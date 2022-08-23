08717079f3d328923ff18b0fa18114131cd5a1e8
Alessandra Costa <alessandracosta8@gmail.com>
2022-08-23 13:08:30 +0000

    Add Model Bakery and dummy post build with it for testing views

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -3,6 +3,7 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth.models import User
 from core.models import Post, Comment, UserProfile
+from model_bakery import baker
 
 
 class TestViews(TestCase):
@@ -16,6 +17,7 @@ class TestViews(TestCase):
             'johnpassword'
             )
         self.client.login(username='john', password='johnpassword')
+        self.post = baker.make(Post)
         self.list_url = reverse('post-list')
         self.detail_url = reverse('post-detail', args=['1'])
 
@@ -25,7 +27,7 @@ class TestViews(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'core/post_list.html')
 
-    # def test_post_detail_view_get(self):
-    #     """ tests that response for GET is positive """
-    #     response = self.client.get(self.detail_url)
-    #     self.assertTemplateUsed(response, 'core/post_detail.html')
+    def test_post_detail_view_get(self):
+        """ tests that response for GET is positive """
+        response = self.client.get(self.detail_url)
+        self.assertTemplateUsed(response, 'core/post_detail.html')

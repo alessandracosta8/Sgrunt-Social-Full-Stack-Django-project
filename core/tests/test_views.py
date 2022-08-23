e74bfd0d096a27ee0880e3954f048563e788733f
Alessandra Costa <alessandracosta8@gmail.com>
2022-08-23 13:36:23 +0000

    Add post edit test fixes

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -1,9 +1,9 @@
 """ Imports: """
 from django.test import TestCase, Client
 from django.urls import reverse
+from django.utils import timezone
 from django.contrib.auth.models import User
 from core.models import Post, Comment, UserProfile
-from model_bakery import baker
 
 
 class TestViews(TestCase):
@@ -17,9 +17,13 @@ class TestViews(TestCase):
             'johnpassword'
             )
         self.client.login(username='john', password='johnpassword')
-        self.post = baker.make(Post)
+        self.post = Post.objects.create(
+            body='text',
+            author=self.user,
+        )
         self.list_url = reverse('post-list')
         self.detail_url = reverse('post-detail', args=['1'])
+        self.edit_url = reverse('post-edit', args=['1'])
 
     def test_post_list_view_get(self):
         """ tests that response for GET is positive """
@@ -30,4 +34,11 @@ class TestViews(TestCase):
     def test_post_detail_view_get(self):
         """ tests that response for GET is positive """
         response = self.client.get(self.detail_url)
+        self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'core/post_detail.html')
+
+    def test_post_edit_view_get(self):
+        """ tests that response for GET is positive """
+        response = self.client.get(self.edit_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'core/post_edit.html')

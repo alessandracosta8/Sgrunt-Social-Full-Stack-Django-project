2481ebf1528ccaef7e7f5e4331470988a9d2828b
Alessandra Costa <alessandracosta8@gmail.com>
2022-08-23 12:59:46 +0000

    Fix from post list view test

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -1,18 +1,31 @@
 """ Imports: """
 from django.test import TestCase, Client
 from django.urls import reverse
+from django.contrib.auth.models import User
 from core.models import Post, Comment, UserProfile
-import json
 
 
 class TestViews(TestCase):
     """ views tests """
 
     def setUp(self):
-        self.post = Post()
+        self.client = Client()
+        self.user = User.objects.create_user(
+            'john',
+            'lennon@thebeatles.com',
+            'johnpassword'
+            )
+        self.client.login(username='john', password='johnpassword')
+        self.list_url = reverse('post-list')
+        self.detail_url = reverse('post-detail', args=['1'])
 
     def test_post_list_view_get(self):
         """ tests that response for GET is positive """
-        response = self.post.get(reverse('post-list'))
+        response = self.client.get(self.list_url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'core/post_list.html')
+
+    # def test_post_detail_view_get(self):
+    #     """ tests that response for GET is positive """
+    #     response = self.client.get(self.detail_url)
+    #     self.assertTemplateUsed(response, 'core/post_detail.html')

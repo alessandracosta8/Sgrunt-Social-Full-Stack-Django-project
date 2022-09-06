8354489853f7c60274fc5b0e867f3d08d903c2d6
Alessandra Costa <alessandracosta8@gmail.com>
2022-09-06 07:27:04 +0000

    Fix typo in settings.py

--- sgrunt_social/settings.py
+++ sgrunt_social/settings.py
@@ -155,7 +155,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 

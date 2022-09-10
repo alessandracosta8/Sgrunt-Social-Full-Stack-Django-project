b8facf11c9f3083c26e1e01f35e4ba34d2b758b3
Alessandra Costa <alessandracosta8@gmail.com>
2022-09-10 15:10:25 +0000

    Fix url.py 404 handler attempt 4

--- sgrunt_social/views.py
+++
@@ -1,7 +0,0 @@
-""" Imports """
-from django.shortcuts import render
-
-
-def page_not_found_view(request, exception):
-    """ 404 customer error view """
-    return render(request, '404.html', status=404)

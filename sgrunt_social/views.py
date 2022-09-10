eaf337ea231260364767323818b4e2cfc5cb333e
Alessandra Costa <alessandracosta8@gmail.com>
2022-09-10 08:57:14 +0000

    Setup view to handle 404

--- sgrunt_social/views.py
+++ sgrunt_social/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 def page_not_found_view(request, exception):
     """ 404 """
-    return render(request, '404.html', status=404)
+    return render(request, '404.html')
